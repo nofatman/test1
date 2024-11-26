89643aae3701e1aa793f7aed0f425b051fc49c09
nofatman <98560346+nofatman@users.noreply.github.com>
2024-11-26 14:09:42 +0800

    Add files via upload

---
+++ appConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "Agc Vercel",
+  tagline: "Free Download Image",
+  description: "Agc is a website auto generate contents.",
+  keywords: "cdom,cdom agc,cdom blog",
+  author: "cDOM",
+  favicon: "https://pugjs.org/images/favicon.ico",
+  imageCount: 10,
+  removeBadWords: "porn,nude,sex",
+};
